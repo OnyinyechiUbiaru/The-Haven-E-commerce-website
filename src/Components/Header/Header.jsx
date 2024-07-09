241d4eca2f974112e88ee049c6445a4cf1fa1f8f
OnyinyechiUbiaru <onyinyechi.ubiaru@gmail.com>
2024-07-09 23:16:16 +0100

    update css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,18 @@
+import frameImage from '../Assets/Frame 3.svg';
 import "./Header.css";
 
 const Header = () => {
   return (
     <section className="hero-section">
-    <div className="header">
+      <img src={frameImage} alt="Background" className="background-image" />
+      <div className="header-content">
         <h1>New Arrivals!!!</h1>
         <h3>Sleep like Royalty: Introducing our New Luxury Bed Collection.</h3>
         <h5>Limited time offer!! Shop our new bed arrivals and enjoy 15% off your first purchase.</h5>
         <button>Shop now</button>
-    </div>
+      </div>
     </section>
-
-    
-  )
+  );
 }
 
-export default Header
+export default Header;
